e939ee97be614b645f85b79ae4ecefb6ab695b60
olajohn2804 <olamoyegunjohn2003@gmail.com>
2022-08-09 08:33:10 +0100

    completed the js for the signUp and signIn page

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var allClients = [];
+getPreviousClients = () => {
+    if(localStorage.localClient){
+        allClients = JSON.parse(localStorage.localClient);
+    }
+}
 // sign up script starts
-reg = () => {
+signUp = () => {
     var FirstName = Firstname.value;
     Firstname.value = "";
     var LastName = Lastname.value;
@@ -25,9 +30,43 @@ reg = () => {
         allClients.push(Client)
         disp.innerHTML = `<div class="alert alert-success text-center">Success,Welcome on board ${Client.firstName}</div>`;
         setTimeout(() => {
-        window.location.href = "index.html";
+        window.location.href = "signIn.html";
         }, 2000);        
     };
     localStorage.localClient = JSON.stringify(allClients);
     // conditionals checking for inputs field ends
 }
+
+// script for sign in starts 
+signIn = () => {
+    signInEmail = email.value;
+    email.value = "";
+    signInPassword = password.value;
+    password.value = "";
+    getClients = JSON.parse(localStorage.getItem("localClient"));
+    for (let i = 0; i < getClients.length; i++) {
+        if(signInEmail == getClients[i].eMail && signInPassword == getClients[i].passWord){
+            disp.innerHTML = `<div class="alert alert-success text-center">Success,Welcome on board ${getClients[i].firstName}</div>`;
+            setTimeout(() => {
+                window.location.href = "index.html"
+            }, 1000);
+        }else if(signInEmail == getClients[i].eMail && signInPassword != getClients[i].passWord){
+            disp.innerHTML = `<div class="alert alert-danger text-center">Incorrect Password</div>`;
+            setTimeout(() => {
+                window.location.href = "signIn.html"
+            }, 2000);
+        }else if(signInEmail != getClients[i].eMail && signInPassword == getClients[i].passWord){
+            disp.innerHTML = `<div class="alert alert-danger text-center">Incorrect Email</div>`;
+            setTimeout(() => {
+                window.location.href = "signIn.html"
+            }, 2000);
+        }else if(signInEmail != getClients[i].eMail && signInPassword != getClients[i].passWord){
+            disp.innerHTML = `<div class="alert alert-danger text-center">Incorrect Email and Password</div>`;
+            setTimeout(() => {
+                window.location.href = "signIn.html"
+            }, 2000);
+        }
+        
+    }
+}
+// signIn script ends
